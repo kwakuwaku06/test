6712c50dde37a9073c8b11fc94d7aead1671d9f2
waku <kwakuwaku06@hotmail.co.jp>
2012-05-05 21:20:42 +0900

    This is php file

---
+++ file.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Created on 2012/05/05
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+ echo 'this is php';
+ 
